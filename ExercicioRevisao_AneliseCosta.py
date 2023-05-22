82b4afd7f0e97cc77804fe341ead29774b23302c
Anelise <froane@gmail.com>
2023-05-22 17:08:49 -0300

    Exercício com questão de revisão a ser resolvida

---
+++ ExercicioRevisao_AneliseCosta.py
@@ -0,0 +1,34 @@
+"""
+Revisão - Prova Bimestral
+
+Você foi designado(a) para desenvolver um código em Python capaz de receber, armazenar e apresentar dados dos
+Estudantes do 3º ano do curso Ensino Médio Integrado em Informática do IFB.
+Para isso, foram definidos os seguintes requisitos negociais:
+    - Ao instanciar a classe Estudante, o usuário deve obrigatoriamente informar matrícula e nome;
+    - Além dos métodos getters/setters, a classe Estudante deve possuir um método para calcular a média individual
+    do aluno(a):
+        - Esse método deve ser capaz de receber 3 notas (A1, A2 e A3) e retornar a média delas. Sendo assim,
+        a classe também precisar ter um atributo para receber o valor da média individual do estudante.
+
+    - Também é requisito negocial, que o código possibilite que enquanto quiser, o usuário continue informado os dados
+    dos estudantes e suas respectivas notas;
+    - Os objetos devem ser salvos em uma estrutura de dados sequencial do tipo mutável chamada "estudantes";
+    - Além disso, cada média individual também deve ser armazenada numa estrutura de dados sequencial do tipo mutável
+    chamada "medias_individuais", que será utilizada como parâmetro para o cálculo da média geral da turma.
+
+    - Para calcular a média geral da turma, é necessário que você crie uma função chamada "calcula_media_geral": capaz
+    de receber uma estrutura de dados sequencial como parâmetro e retornar a média de seus valores.
+
+    - Após a obtenção dos dados dos estudantes e cálculos de médias (individual e geral), o código deve apresentar na tela:
+        - Matrícula, nome e média individual de cada estudante cadastrado; e
+        - Média geral da turma;
+
+    Obs.:
+    - Os valores de média devem ser formatados com 2 casas decimais.
+    - O arquivo a ser enviado deve possuir as extensões .py OU .txt;
+    - A nomenclatura do arquivo deve seguir obrigatoriamente o seguinte padrão: NumeroExercicio_nomeUltimoNome.py.
+    Ex.: ExercicioRevisao_AnaMartins.py ou ExercicioRevisao_AnaMartins.txt.
+    (Exercícios enviados fora do padrão de nomenclatura serão desconsiderados para contabilização de nota)
+  """
+
+
