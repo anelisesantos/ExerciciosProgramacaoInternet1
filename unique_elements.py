dfdb298cab42e6da19daada8b830c467c816c343
Anelise Costa dos Santos <55819077+anelisesantos@users.noreply.github.com>
2023-12-08 10:10:58 -0300

    Add files via upload
    Adiciona funcionalidade para extrair elementos únicos de uma lista
    
    Este commit melhora o script para que ele crie uma nova lista contendo apenas elementos únicos da lista original. O código foi corrigido para incluir uma verificação condicional dentro do loop for, garantindo que apenas elementos não duplicados sejam adicionados à nova lista. Além disso, a impressão final foi atualizada para exibir a nova lista.

---
+++ unique_elements.py
@@ -0,0 +1,14 @@
+﻿# Inicializa a lista original e a nova lista
+my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+new_list = []
+
+# Percorre cada elemento na lista original
+for i in my_list:
+    # Verifica se o elemento não está na nova lista
+    if i not in new_list:
+        # Adiciona o elemento à nova lista se ele for único
+        new_list.append(i)
+
+# Imprime a nova lista com elementos únicos
+print("The list with unique elements only:")
+print(new_list)
